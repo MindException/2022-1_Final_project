856f7114729332a53290c47c3cb941c70db0cf26
MindException <tourer97@naver.com>
2022-03-06 22:59:25 +0900

    프로젝트 생성(파이어베이스 연동 성공)

---
+++ app/src/main/java/com/lastproject/used_item_market/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lastproject.used_item_market;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
