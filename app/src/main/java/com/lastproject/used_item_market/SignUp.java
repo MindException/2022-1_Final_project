b6f49770881b59c4522b50519e06fef9813f80df
MindException <tourer97@naver.com>
2022-04-28 14:06:07 +0900

    0428 NewDB 닉네임 중복 검사

--- app/src/main/java/com/lastproject/used_item_market/SignUp.java
+++ app/src/main/java/com/lastproject/used_item_market/SignUp.java
@@ -14,20 +14,26 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class SignUp extends AppCompatActivity {
 
     //구글 이메일
     String email;
 
-    //서버 관련
-    private FirebaseDatabase database;
-    private DatabaseReference myRef;
+    //새서버 관련
+    private FirebaseFirestore firestore;
+    private CollectionReference userDocument;
     public User userinfo;           //서버에 저장될 사용자의 정보
 
     //EditText
@@ -46,10 +52,6 @@ public class SignUp extends AppCompatActivity {
 
     //trigger 모음
     boolean password_trigger = false;                   //2번째 재확인 비밀번호가 맞는지 확인하는 용
-    boolean email_trigger = false;                      //이미 있는 아이디인지 확인용
-    boolean nickname_trigger = false;                   //이미 있는 닉네임인지 확인용
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,12 +59,11 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.sign_up);
 
         //서버 연결
-        database = FirebaseDatabase.getInstance();
-        myRef = database.getReference();
+        firestore = FirebaseFirestore.getInstance();
+        userDocument = firestore.collection("User");
 
         //인증에 성공한 이메일 값을 받아온다.
         email = getIntent().getStringExtra("email");
-        System.out.println(email);
 
         //EditText 연결
         et_email = (EditText) findViewById(R.id.email_set);
@@ -74,46 +75,6 @@ public class SignUp extends AppCompatActivity {
         //이메일 자동 세팅(xml에서 못 건드리게해서 고정된다.)
         et_email.setText(email);
 
-        //구글 아이디를 검색하여 이미 회원가입이 되어 있는 사용자인지 확인한다.
-        try{
-
-            myRef.child("User").addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {      //이미 회원가입한 아이디인지 확인한다.
-
-                    for(DataSnapshot ds1 : snapshot.getChildren()){
-
-                        User checkuser = ds1.getValue(User.class);           //아이디가 있는지 확인한다.
-                        if(email.equals(checkuser.google_email)){                //아이디가 이미 존재하는 경우
-
-                            Toast.makeText(SignUp.this, "회원가입한 아이디입니다.", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(SignUp.this, Login.class);
-                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                            email_trigger = true;
-                            startActivity(intent);
-                            break;
-
-                        }
-
-                    }//for문 끝
-
-                    if(email_trigger == true){      //중복 이메일 발생하여 액티비티 종료
-                        System.exit(0);
-                    }
-
-
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-
-                }
-            });
-
-
-
-        }catch (Exception e){ }
 
         //텍스트 갱신
         et_password2.addTextChangedListener(new TextWatcher() {
@@ -148,9 +109,6 @@ public class SignUp extends AppCompatActivity {
 
             }
         });
-
-
-
 
 
         //뒤로가기 버튼
@@ -194,51 +152,33 @@ public class SignUp extends AppCompatActivity {
                     return;     //비밀번호가 일치하지 않는다.
                 }
 
-                //닉네임이 있는지 검사한다.
-                myRef.child("User").addListenerForSingleValueEvent(new ValueEventListener() {
+                sname = et_name.getText().toString();           //닉네임을 가져온다.
+
+                //닉네임을 검사한다.
+                Query query = userDocument.whereEqualTo("nickname", sname);
+                query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){        //송수신 성공시
+                            if(task.getResult().size() <= 0){       //아무것도 없어야 중복이 아니다.
 
-                        sname = et_name.getText().toString();           //닉네임을 가져온다.
+                                Intent signUp_intent = new Intent(SignUp.this, SignUpMap.class);
+                                signUp_intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                signUp_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                signUp_intent.putExtra("email", email);
+                                signUp_intent.putExtra("password",spassword1);
+                                signUp_intent.putExtra("nickname",sname);
+                                startActivity(signUp_intent);
+                                System.exit(0);
 
-                        for(DataSnapshot ds1 : snapshot.getChildren()){     //닉네임 검사
-
-                            User check_user = ds1.getValue(User.class);
-                            if(sname.equals(check_user.nickname)){                   //닉네임이 같을 경우
-
-                                nickname_trigger = true;
-                                et_name.setText("");
-                                et_name.setHint("이미 존재하는 닉네임입니다.");
-
+                            }else{
+                                Toast.makeText(SignUp.this, "닉네임 중복", Toast.LENGTH_SHORT).show();
                             }
-
-
-
-                        }//for문 끝
-
-                        if(nickname_trigger == false){       //존재하는 닉네임이 아닐 경우
-
-                            Intent signUp_intent = new Intent(SignUp.this, SignUpMap.class);
-                            signUp_intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                            signUp_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                            signUp_intent.putExtra("email", email);
-                            signUp_intent.putExtra("password",spassword1);
-                            signUp_intent.putExtra("nickname",sname);
-                            startActivity(signUp_intent);
-                            finish();
-
                         }
-                        nickname_trigger = false;
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
                     }
                 });
 
-            }
+            }//onClick
         });
 
 
